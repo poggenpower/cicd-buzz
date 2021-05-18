35bbc2f007b136f4ee787476c9ba000de47af1b1
thl@deb030 <thl@deb030>
2021-05-18 02:04:51 +0200

    image name

--- .travis/deploy_kubeernetes.sh
+++ .travis/deploy_kubeernetes.sh
@@ -2,7 +2,7 @@
 pwd
 ls -la 
 DOCKER_IMAGE=$(cat docker_image.txt)
-sed "s#image: poggenpower/cicd-buzz:latest#image: ${DOCKER_USER}/${DOCKER_IMAGE}#" deployment.yaml > deployment.tmp.yaml
+sed "s#image: poggenpower/cicd-buzz:latest#image: ${DOCKER_IMAGE}#" deployment.yaml > deployment.tmp.yaml
 cat deployment.tmp.yaml
 kubectl --insecure-skip-tls-verify=true apply -f deployment.tmp.yaml 
 cat README.md
